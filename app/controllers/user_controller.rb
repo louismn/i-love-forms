da89972359b041e50c64adb2c75b5f9c4f3a728e
louismn <louis.martindunord@eemi.com>
2018-10-30 15:59:30 +0100

    Avancer sur le formulaire

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,8 +1,9 @@
 class UserController < ApplicationController
   def new
+  	@user = User.new
   end
 
   def create
-  	user = User.create(username: params["username"],email: params["email"], bio: params["bio"])
+  	@user = User.create(username: params["username"],email: params["email"], bio: params["bio"])
   end
 end
